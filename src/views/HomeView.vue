0dd114c4bebfecdd1a892c0b1da7c50ce1f98aa1
Maman <novita.vsalama@gmail.com>
2022-12-03 23:29:14 +0700

    setup tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-serif m-4 font-bold text-red-600">
+      heading tailwind
+    </h1>
   </main>
 </template>
